e92faca0a1f77ec84cbe595fe0a28245b9b5555f
glenndomin <glenn.domin@ose.com.au>
2019-08-21 18:01:01 +1000

    doing the round list

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,20 @@
   <main class="container above-fold">
     <section>
       <h1 class="title">Glenn Domin</h1>
-      <h2 class="subtitle">Front-end developer.</h2>
+      <h2 class="subtitle">A Front-end developer who makes websites 
+        <ul>
+          <li>semantic</li>
+          <li>performant</li>
+          <li>efficient</li>
+          <li>modular</li>
+          <li>scalable</li>
+          <li>accessible</li>
+          <li>aesthetic</li>
+          <li>secure</li>
+          <li>standards-compliant</li>
+          <li>bullet-proof</li>
+        </ul>
+      </h2>
       <button class="btn" @click="showModal = true">About Me</button>
     </section>
     <shapes />
@@ -28,7 +41,7 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .above-fold {
   margin: auto;
   min-height: 100vh;
@@ -43,6 +56,44 @@ export default {
   width: 100%;
   height: 100%;
 }
+
+.subtitle {
+
+  ul {
+    display: inline-block;
+    position: relative;
+    list-style: none;
+    padding-left: 0;
+    margin-bottom: 0;
+    vertical-align: top;
+    overflow: hidden;
+    height: 2rem;
+
+    &::after {
+      content: 'semantic';
+      display: block;
+      animation: listUp 20s linear 1s infinite;
+    }
+
+    li {
+      animation: listUp 20s linear 1s infinite;
+    }
+  }
+}
+
+@keyframes listUp {
+  0%        { transform: translateY(0px); }
+  5%, 10%   { transform: translateY(-1.5em); }
+  15%, 20%  { transform: translateY(-3em); }
+  25%, 30%  { transform: translateY(-4.5em); }
+  35%, 40%  { transform: translateY(-6em); }
+  45%, 50%  { transform: translateY(-7.5em); }
+  55%, 60%  { transform: translateY(-9em); }
+  65%, 70%  { transform: translateY(-10.5em); }
+  75%, 80%  { transform: translateY(-12em); }
+  85%, 90%  { transform: translateY(-13.5em); }
+  95%, 100% { transform: translateY(-15em); }
+}
 </style>
 
 <style>
@@ -54,6 +105,6 @@ export default {
   margin-bottom: 1.75rem;
   font-style: italic;
   font-weight: 400;
-  font-size: 1.5rem;
+  font-size: 1.25rem;
 }
 </style>
