2357ef2691738d15f5ae977c096d9bb97c26c89e
glenndomin <glenn.domin@ose.com.au>
2019-08-25 16:47:04 +1000

    attempting to fix form issue

--- static/form.vue
+++ static/form.vue
@@ -1,8 +1,8 @@
 <template>
   <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field" hidden>
-    <input id="name" type="text" name="name" placeholder=" " class="form-control" required />
-    <input id="email" type="email" name="email" placeholder=" " class="form-control" required />
-    <textarea id="message" name="message" placeholder=" " class="form-control" required></textarea>
+    <input type="text" name="name" placeholder=" " class="form-control" required />
+    <input type="email" name="email" placeholder=" " class="form-control" required />
+    <textarea name="message" placeholder=" " class="form-control" required></textarea>
     <button type="submit" class="btn">Send</button>
   </form>
 </template>
