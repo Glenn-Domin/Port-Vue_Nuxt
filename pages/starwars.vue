d99f593571603bbe065a815c887378adfb4c9f79
glenndomin <glenn.domin@ose.com.au>
2020-09-27 17:59:24 +1000

    star wars!

---
+++ pages/starwars.vue
@@ -0,0 +1,105 @@
+<template>
+  <main class="content container page-starwars">
+    <header class="page-heading">
+      <h1 class="title text-center">Character - Force and Destiny</h1>
+      <p class="subtitle text-center">Vanzer Madaar</p>
+    </header>
+    <section>
+      <h2>Backstory</h2>
+      <figure class="figure-right">
+        <img src="~/static/images/starwars/Quolas-Elrit-Tree.png" class="img" width="333.33" height="211" alt="Elrit Tree">
+        <figcaption>The Elrit Tree, Quolas</figcaption>
+      </figure>
+      <p>Vanzer Madaar was part of a group of Sathari peace-keepers on his homeworld of Arbooine in the Outer Rim. He lived in the great tree-city of Quolas, built into the boughs of a massive elrit tree. There was much feuding between the powerful Helshar and Tumris clans. Vanzer's group of peace-keepers would attempt to prevent disagreements between members of these clans from escalating into bloodshed.</p>
+      <p>Vanzer originally had positive feelings toward the Jedi order, and associated with their tenets of peace-keeping, though by the time Vanzer learned of his force powers he was already too old to be considered to join the order. Instead he became a peace-keeper, and tried to develop his force powers alone. He saught to follow the approach the Jedi took toward problem-solving, preferring to engage with words, considering violence as a last resort. Through hearing stories of the Jedi and the Sith he became keenly aware of the pull of the dark side. He considered the protection of others above the advancement of his own developing force powers and saw the dark side as a corruptive influence, predicting it would lead to animosity and hatred, and so he never persued it. He likened it to the worst of the behaviours he observed between the Helshar and Tumris clans, and saw only pointless destruction if he went down that path.</p>
+      <p>The Jedi came to Arbooine during the clone wars, seeking an alliance. Arbooine had remained neutral in their conflict with the separatists, wanting to keep out of the war. After the Jedi arrived, a separatist fleet thought Arbooine was going to side with the Jedi, and began a bombardment of the planet, causing innumerable casualties. Vanzer was part of a group that managed to secure a ship and rapidly fled, and later began a search for a new home.</p>
+      <p>Due to the Jedi bringing war to his world, as well as the militarisation of the Jedi not fitting their role as peace-keepers, Vanzer became disillusioned with the order, feeling they had lost their way.</p>
+      <p>Today he tries to maintain a low profile, keeping front-of-mind the knowledge that his force powers would attract the attention of the new Galactic Empire.</p>
+    </section>
+    <hr>
+    <section>
+      <h2>Character Traits</h2>
+      <figure class="figure-right">
+        <img src="~/static/images/starwars/Sathari.png" class="img" width="300" height="340.33" alt="Sathari">
+        <figcaption>A Sathari</figcaption>
+      </figure>
+      <h3>Species - Sathari</h3>
+      <p>
+        <a href="https://starwars.fandom.com/wiki/Sathari" target="_blank" title="Sathari Lore">Described in Legends</a> as a <q>reclusive sentient avian species</q>.
+        The Sathari capital of <a href="https://starwars.fandom.com/wiki/Quolas" target="_blank" title="More on Quolas">Quolas</a> seems particularly interesting, in that it's built up into a massive tree.
+        As the homeworld of <a href="https://starwars.fandom.com/wiki/Arbooine" target="_blank" title="More on Arbooine">Arbooine</a> has already had politcal interference from the Jedi in legends, I think it's not too much of a stretch to bring the Jedi/Separatist conflict there full-force in Vanzer's backstory.
+      </p>
+      <p style="margin-bottom: 0;"><a href="https://star-wars-rpg-ffg.fandom.com/wiki/Sathari" target="_blank" title="See Stats">Sathari have the following stats</a>:</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Brawn</th>
+            <th>Agility</th>
+            <th>Intellect</th>
+            <th>Cunning</th>
+            <th>Willpower</th>
+            <th>Presence</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+      <p>
+        <b>Wound Threshold:</b> 8 + Brawn <br>
+        <b>Strain Threshold:</b> 10 + Willpower <br>
+        <b>Starting Experience:</b> 100 XP <br>
+        <b>Special Abilities:</b> Coordination +1 <br>
+        <b>Glider: </b>Can fall safely from any height and can glide over small gaps or distance while falling.
+      </p>
+      <hr>
+      <h3>Career - Consular Arbiter</h3>
+      <p>The description of <a href="https://star-wars-rpg-ffg.fandom.com/wiki/Category:Consular" target="_blank">the Consular</a> on the Fandom wiki as <q>a force sensitive character that prefer a peaceful resolution to any conflict</q> seems in keeping with the backstory above, with the character repulsed by the militarisation of the Jedi order and the character being drawn to the more traditional peace-keeping role of the Jedi.</p>
+      <p>Similarly, <a href="https://star-wars-rpg-ffg.fandom.com/wiki/Arbiter" target="_blank">the Arbiter's</a> described as someone <q>who seeks to resolve situations in a calm and respectful manner</q>, which also seems to fit thematically. The skill tree also appeals to me, both containing bonuses to talking and fighting. It's also one of the few specialisations with the lightsaber as a bonus career skill, which I figure will come in handy.</p>
+      <p>The career bonuses between Consular and Arbiter seem great. There's a lot of knowledge, perception, lightsaber proficiency and cool, all which I think could come in handy.</p>
+      <img src="~/static/images/starwars/Arbiter.png" class="img" width="798" height="1036" alt="Arbiter Specialisation Sheet" style="margin-left: auto; margin-right: auto; display: block;">
+    </section>
+    <hr>
+  </main>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+.page-starwars {
+  line-height: 1.8;
+}
+th, td {
+  padding-right: 12px;
+  padding-bottom: 12px;
+}
+hr {
+  margin: 4rem 3rem;
+  border-width: 1px;
+  border-bottom: none;
+  border-color: #fff;
+}
+.figure-right {
+  float: right;
+  margin-left: 8px;
+  margin-bottom: 8px;
+  border: 1px solid #aaa;
+}
+figure figcaption {
+  padding: 5px;
+  font-style: italic;
+  font-size: 90%;
+}
+.img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
