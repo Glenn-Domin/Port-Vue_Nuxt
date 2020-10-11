c211b0eca77083041b9c8c2b2cabb0dee1356c9f
glenndomin <glenn.domin@ose.com.au>
2020-10-11 18:10:59 +1000

    star wars stuff

--- pages/starwars.vue
+++ pages/starwars.vue
@@ -7,20 +7,53 @@
     <section>
       <h2>Backstory</h2>
       <figure class="figure-right">
-        <img src="~/static/images/starwars/Quolas-Elrit-Tree.png" class="img" width="333.33" height="211" alt="Elrit Tree">
+        <img src="~/static/images/starwars/Quolas-Elrit-Tree.png" class="img" width="333.33" height="211" alt="Elrit Tree" loading="lazy">
         <figcaption>The Elrit Tree, Quolas</figcaption>
       </figure>
       <p>Vanzer Madaar was part of a group of Sathari peace-keepers on his homeworld of Arbooine in the Outer Rim. He lived in the great tree-city of Quolas, built into the boughs of a massive elrit tree. There was much feuding between the powerful Helshar and Tumris clans. Vanzer's group of peace-keepers would attempt to prevent disagreements between members of these clans from escalating into bloodshed.</p>
       <p>Vanzer originally had positive feelings toward the Jedi order, and associated with their tenets of peace-keeping, though by the time Vanzer learned of his force powers he was already too old to be considered to join the order. Instead he became a peace-keeper, and tried to develop his force powers alone. He saught to follow the approach the Jedi took toward problem-solving, preferring to engage with words, considering violence as a last resort. Through hearing stories of the Jedi and the Sith he became keenly aware of the pull of the dark side. He considered the protection of others above the advancement of his own developing force powers and saw the dark side as a corruptive influence, predicting it would lead to animosity and hatred, and so he never persued it. He likened it to the worst of the behaviours he observed between the Helshar and Tumris clans, and saw only pointless destruction if he went down that path.</p>
-      <p>The Jedi came to Arbooine during the clone wars, seeking an alliance. Arbooine had remained neutral in their conflict with the separatists, wanting to keep out of the war. After the Jedi arrived, a separatist fleet thought Arbooine was going to side with the Jedi, and began a bombardment of the planet, causing innumerable casualties. Vanzer was part of a group that managed to secure a ship and rapidly fled, and later began a search for a new home.</p>
+      <p>The Jedi, on behalf of the Republic, came to Arbooine during the clone wars, seeking an alliance. Arbooine had remained neutral in their conflict with the separatists, wanting to keep out of the war. After the Jedi arrived, a separatist fleet thought Arbooine was going to side with the Republic, and began a bombardment of the planet, causing innumerable casualties. Vanzer was part of a group that managed to secure a ship and rapidly fled the planet. They later began a search for a new home.</p>
       <p>Due to the Jedi bringing war to his world, as well as the militarisation of the Jedi not fitting their role as peace-keepers, Vanzer became disillusioned with the order, feeling they had lost their way.</p>
       <p>Today he tries to maintain a low profile, keeping front-of-mind the knowledge that his force powers would attract the attention of the new Galactic Empire.</p>
+      <section>
+        <h3>Character Perspectives</h3>
+        <ul>
+          <li>
+            <h4>The Jedi Order</h4>
+            <p>Vanzer has an ambivalence toward the order today. On one hand they brought war to his world, and on the other he respected their tenets and admired them in the past, initially seeking to join them. Former members of the order may get a complicated reception. He seeks to learn more about the force, but may balk at their order's more recent history.</p>
+          </li>
+          <li>
+            <h4>The Separatists</h4>
+            <p>While Vanzer's perspective on the Jedi Order was nuanced, his views on the Separatists are far more straightforward. He hated the Separatists for bombarding his homeworld.</p>
+          </li>
+          <li>
+            <h4>The Sith</h4>
+            <p>Vanzer sees the Sith as morally bankrupt, putting their own selfish power ahead of everything else. He sees this as pointless, as power for power's sake has no purpose.</p>
+          </li>
+          <li>
+            <h4>The Republic</h4>
+            <p>Vanzer sees the Republic as responsible for the destruction of his homeworld along with the Jedi Order and the Separatists, seeing them as uncaring. They used entire planetary systems as pawns in a galactic conflict and so there's no love lost from the end of the Republic.</p>
+          </li>
+          <li>
+            <h4>The Empire</h4>
+            <p>In the Empire Vanzer sees the worst parts of the former Republic magnified; a brutal militaristic conquest of the galaxy causing mass-upheaval and countless deaths. There also isn't even the pretense of democracy or concern for the citizens of the galaxy. Vanzer can keep his displeasure quiet in order to get by in the galaxy but he's not at all pleased with the current state of affairs.</p>
+          </li>
+          <li>
+            <h4>The Sathari</h4>
+            <p>Vanzer feels very protective of the Sathari, continuing his peace-keeper mindset from his time on Arbooine. He was part of a group of relatively few Sathari who fled Arbooine and has grave concern for the future of his species.</p>
+          </li>
+          <li>
+            <h4>R</h4>
+            <p>Vanzer doesn't quite know what to make of R yet. They've gotten along well in their communications so far but R has kept is cards very close to his chest, and this has not gone unnoticed. Vanzer's willing to go along with R's request for help for now, but expects answers soon.</p>
+          </li>
+        </ul>
+      </section>
     </section>
     <hr>
     <section>
       <h2>Character Traits</h2>
       <figure class="figure-right">
-        <img src="~/static/images/starwars/Sathari.png" class="img" width="300" height="340.33" alt="Sathari">
+        <img src="~/static/images/starwars/Sathari.png" class="img" width="300" height="340.33" alt="Sathari" loading="lazy">
         <figcaption>A Sathari</figcaption>
       </figure>
       <h3>Species - Sathari</h3>
@@ -64,7 +97,39 @@
       <p>The description of <a href="https://star-wars-rpg-ffg.fandom.com/wiki/Category:Consular" target="_blank">the Consular</a> on the Fandom wiki as <q>a force sensitive character that prefer a peaceful resolution to any conflict</q> seems in keeping with the backstory above, with the character repulsed by the militarisation of the Jedi order and the character being drawn to the more traditional peace-keeping role of the Jedi.</p>
       <p>Similarly, <a href="https://star-wars-rpg-ffg.fandom.com/wiki/Arbiter" target="_blank">the Arbiter's</a> described as someone <q>who seeks to resolve situations in a calm and respectful manner</q>, which also seems to fit thematically. The skill tree also appeals to me, both containing bonuses to talking and fighting. It's also one of the few specialisations with the lightsaber as a bonus career skill, which I figure will come in handy.</p>
       <p>The career bonuses between Consular and Arbiter seem great. There's a lot of knowledge, perception, lightsaber proficiency and cool, all which I think could come in handy.</p>
-      <img src="~/static/images/starwars/Arbiter.png" class="img" width="798" height="1036" alt="Arbiter Specialisation Sheet" style="margin-left: auto; margin-right: auto; display: block;">
+      <img src="~/static/images/starwars/Arbiter.png" class="img" width="798" height="1036" alt="Arbiter Specialisation Sheet" style="margin-left: auto; margin-right: auto; display: block;" loading="lazy">
+    </section>
+    <hr>
+    <section>
+      <h2>More on Arbooine</h2>
+      <h3>Stats (prior to planetary bombardment)</h3>
+      <p>
+        <b>Astronavigational Data</b>: Arboo system, Arboo sub-sector, Outer Rim region <br>
+        <b>Orbital Metrics</b>: 413 days per year / 28 hours per day <br>
+        <b>Government</b>: locally ruled settlements <br>
+        <b>Population</b>: 70 million (99% Sathari, 1% other) <br>
+        <b>Languages</b>: Arbori, Basic <br>
+        <b>Terrain</b>: forests, inland seas <br>
+        <b>Major Cities</b>: Zeilo (capital), Quolas, Porthor <br>
+        <b>Areas of Interest</b>: tree-cities <br>
+        <b>Major Exports</b>: wood and woodwork <br>
+        <b>Major Imports</b>: technology <br>
+        <b>Trade Routes</b>: Diophos Spur
+      </p>
+      <h3>Background</h3>
+      <figure class="figure-right">
+        <img src="~/static/images/starwars/Pillar_of_the_Ancestors_CotG.png" class="img" width="381" height="487.5" alt="Pillar of the Ancestors" loading="lazy">
+        <figcaption>Pillar of the Ancestors</figcaption>
+      </figure>
+      <p>Arbooine is covered almost entirely in dense and impenetrable temperate, ancient-growth forests and topped with small icecaps at both poles. It has no true oceans to speak of, but it has countless freshwater rivers and lakes, varying in size from small pools to inland seas. the planet is home to many native species, including harmless herbivores, such as the utarvix, and dangerous predators, like the venomous bearsloth.</p>
+
+      <p>Out of all of Arbooine’s native life, only a singular sentient species called it home. The Sathari are a humanoid species of avian descent who have lived on Arbooine for countless eons. The Sathari are an insular people who declined to take any noticeable role in galactic politics.</p>
+
+      <p>Arbooine’s sentient population was relatively small; Republic estimates placed it at roughly 70 million, sparsely distributed. The planet’s largest and most dense settlement by far was its capitol city of Zeilo, also home to Arbooine’s only spaceport. Other settlements are scattered across the planet’s surface, many established in the vast elrit trees. Still others range from villages of wooden houses to domes of metal and stone to nomadic tent-camps.</p>
+
+      <p>The name “Arbooine” was given to the planet by an uninspired Republic cartographer when the Sathari name for the world proved unpronounceable in Basic (and without a beak). The planet came to the attention of the Republic High Command during the Clone Wars because of its proximity to the Diophos Spur, a little-used route into Separatist space. The Grand Army of the Republic planned to use Arbooine as a staging post for a surprise assault into Separatist territory, but the Separatists discovered the plot and bombarded the planet. Most inhabitants died, while a minority managed to flee the planet.</p>
+
+      <p>Since that time, what's left of Arbooine has once again faded from the galactic stage. Although Arbooine is part of the Empire, Imperial forces are seldom seen there. However, within the last year, Imperial Command has determined that Arbooine’s remoteness and countless kilometers of dense forest make it a possible location for a hidden Rebel base. In response to the recent Rebel attacks in the region, Imperial forces have been dispatched to search and secure the planet as needed.</p>
     </section>
     <hr>
   </main>
@@ -92,6 +157,10 @@ hr {
   margin-left: 8px;
   margin-bottom: 8px;
   border: 1px solid #aaa;
+
+  @media (min-width: 768px) {
+    margin-left: 20px;
+  }
 }
 figure figcaption {
   padding: 5px;
@@ -101,5 +170,16 @@ figure figcaption {
 .img {
   max-width: 100%;
   height: auto;
+}
+ul {
+
+  li::marker {
+    font-size: 1.4rem;
+    color: #FF0A78;
+  }
+
+  h4 {
+    margin-bottom: 0;
+  }
 }
 </style>
